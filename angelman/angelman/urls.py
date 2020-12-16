2fb674badec9056ae4c63540bcfda1b01416ebbb
ScriptSmith <ScriptSmith@users.noreply.github.com>
2020-12-16 15:44:42 +1000

    Remove angelman-specific parent views

--- angelman/angelman/urls.py
+++ angelman/angelman/urls.py
@@ -2,13 +2,7 @@ from django.conf.urls import include
 from django.urls import re_path
 from django.views.generic import RedirectView
 
-from . import parent_view
-
 urlpatterns = [
     re_path(r'^$', RedirectView.as_view(url='router/', permanent=False)),
-    re_path(r"^(?P<registry_code>\w+)/parent/?$",
-            parent_view.ParentView.as_view(), name='parent_page'),
-    re_path(r"^(?P<registry_code>\w+)/parent/(?P<parent_id>\d+)/?$",
-            parent_view.ParentEditView.as_view(), name='parent_edit'),
     re_path(r'', include('rdrf.urls')),
 ]
