80acb77fab93a20c750b32287ac60690958ae5ef
Alin Treznai <alin.treznai.gemini@gmail.com>
2020-12-29 13:47:52 +0200

    Also calculate form progress for fixed clinical data entries

--- angelman/angelman/management/commands/fix_clinical_data_contexts.py
+++ angelman/angelman/management/commands/fix_clinical_data_contexts.py
@@ -1,15 +1,16 @@
 from collections import defaultdict
-import re
 import sys
 
 from django.contrib.contenttypes.models import ContentType
 from django.core.management.base import BaseCommand
 from rdrf.models.definition.models import RDRFContext, Registry, ClinicalData, ContextFormGroupItem
+from rdrf.db.dynamic_data import DynamicDataWrapper
 from registry.patients.models import Patient
 
 
 class Command(BaseCommand):
-    help = "Fixes imported clinical data entries which do not have the context_id field set"
+    help = """Fixes imported clinical data entries which do not have the context_id field set.
+              Also computes form progress for fixed cdes collections"""
 
     def add_arguments(self, parser):
         parser.add_argument("registry_code")
@@ -34,30 +35,21 @@ class Command(BaseCommand):
             ClinicalData.objects.filter(
                 registry_code=reg_code, django_model='Patient',
                 context_id__isnull=True
-            ).order_by('-id').values('id', 'django_id', 'collection', 'data'))
+            ).order_by('-id'))
         self.stdout.write(f"Found {cd_models.count()} entries to fix")
-        result = defaultdict(list)
-        data_dict = {}
         cfgs_map = {}
         for cfg in ContextFormGroupItem.objects.filter(registry_form__registry=self.registry_model):
             cfgs_map[cfg.registry_form.name] = cfg.context_form_group.id
-        print(f"CFGs map={cfgs_map}")
+        self.stdout.write(f"CFGs map={cfgs_map}")
+        patients_and_contexts = defaultdict(list)
         for entry in cd_models:
-            if entry["collection"] != "progress":
-                form_names = [f["name"] for f in entry["data"]["forms"]]
-                print(f"Forms={form_names}")
-            else:
-                data = entry["data"]
-                for k, v in data.items():
-                    if v and k.endswith("form_current"):
-                        search_result = re.search('(.+)_form_current', k)
-                        if search_result:
-                            form_names = [search_result.groups()[0]]
-                print(f"Progress forms={form_names}")
-
+            if entry.collection == "progress":
+                continue
+            form_names = [f["name"] for f in entry.data["forms"]]
+            self.stdout.write(f"Forms={form_names}")
             cfgs = list(set(cfgs_map[f] for f in form_names if f in cfgs_map))
-            print(f"CFGs={cfgs}")
-            patient_id = entry["django_id"]
+            self.stdout.write(f"CFGs={cfgs}")
+            patient_id = entry.django_id
             patient = Patient.objects.filter(pk=patient_id).first()
             if patient:
                 content_type = ContentType.objects.get_for_model(patient)
@@ -71,12 +63,19 @@ class Command(BaseCommand):
                                                          object_id=patient_id,
                                                          context_form_group_id=cfgs[0])
 
-                result[context.id].append(entry['id'])
-                data = entry["data"]
-                data["context_id"] = context.id
-                data_dict[entry['id']] = data
+                entry.data["context_id"] = context.id
+                entry.context_id = context.id
+                entry.save()
+                if entry.collection == "cdes":
+                    patients_and_contexts[patient].append(context)
 
-        for context_id, ids in result.items():
-            ClinicalData.objects.filter(pk__in=ids).update(context_id=context_id)
-        for entry_id, data in data_dict.items():
-            ClinicalData.objects.filter(pk=entry_id).update(data=data)
+        self.stdout.write("Calculating form progress...")
+        seen = set()
+        for patient, contexts in patients_and_contexts.items():
+            for c in contexts:
+                if (patient.id, c.id) in seen:
+                    continue
+                self.stdout.write(f"Calculating progress for patient {patient.id} and contex {c.id}")
+                dyn_patient = DynamicDataWrapper(patient, rdrf_context_id=c.id)
+                dyn_patient.save_form_progress(self.registry_model.code, context_model=c)
+                seen.add((patient.id, c.id))
