2571559685f636aaa5261ed1d85e5e35a13eb14d
Alin Treznai <alin.treznai.gemini@gmail.com>
2021-03-08 10:24:08 +0200

    Ensure patients have a default context created

--- angelman/angelman/management/commands/fix_clinical_data_contexts.py
+++ angelman/angelman/management/commands/fix_clinical_data_contexts.py
@@ -6,6 +6,7 @@ from django import db
 from django.contrib.contenttypes.models import ContentType
 from django.core.management.base import BaseCommand
 from rdrf.models.definition.models import RDRFContext, Registry, ClinicalData, ContextFormGroupItem
+from rdrf.db.contexts_api import RDRFContextManager
 from rdrf.db.dynamic_data import DynamicDataWrapper
 from registry.patients.models import Patient
 
@@ -13,15 +14,12 @@ import logging
 
 logger = logging.getLogger(__name__)
 
-def calculate_progress(registry_code, patient_id, context_id):
-    logger.info(f"Calculating {patient_id=} {context_id=}")
-    patient = Patient.objects.get(pk=patient_id)
-    context = RDRFContext.objects.get(pk=context_id)
 
+def calculate_progress(registry_code, patient, context):
+    logger.info(f"Calculating {patient} with context id {context.id}")
     dyn_patient = DynamicDataWrapper(patient, rdrf_context_id=context.id)
     dyn_patient.save_form_progress(registry_code, context_model=context)
-    logger.info(f"Calculated {patient.id=} {context.id=}")
-
+    logger.info(f"Calculated {patient} with context id {context.id}")
 
 
 class Command(BaseCommand):
@@ -49,6 +47,7 @@ class Command(BaseCommand):
 
     def _fix_entries(self):
         reg_code = self.registry_model.code
+        context_mgr = RDRFContextManager(self.registry_model)
         cd_models = (
             ClinicalData.objects.filter(
                 registry_code=reg_code, django_model='Patient',
@@ -60,24 +59,31 @@ class Command(BaseCommand):
             cfgs_map[cfg.registry_form.name] = cfg.context_form_group.id
         self.stdout.write(f"CFGs map={cfgs_map}")
         patients_and_contexts = defaultdict(list)
+        with_default_contexts_created = set()
         for entry in cd_models:
             if entry.collection != "cdes":
                 continue
             form_names = [f["name"] for f in entry.data["forms"]]
-            self.stdout.write(f"Forms={form_names}")
+            # self.stdout.write(f"Forms={form_names}")
             cfgs = list(set(cfgs_map[f] for f in form_names if f in cfgs_map))
-            self.stdout.write(f"CFGs={cfgs}")
+            # self.stdout.write(f"CFGs={cfgs}")
             if len(cfgs) == 0:
                 continue
             patient_id = entry.django_id
             patient = Patient.objects.filter(pk=patient_id).first()
             if patient:
+                if patient_id not in with_default_contexts_created:
+                    # Ensure all fixed context are created
+                    context_mgr.get_or_create_default_context(patient)
+                    with_default_contexts_created.add(patient_id)
+
                 content_type = ContentType.objects.get_for_model(patient)
                 context = RDRFContext.objects.filter(registry=self.registry_model,
                                                      content_type=content_type,
                                                      object_id=patient_id,
                                                      context_form_group_id=cfgs[0]).first()
                 if not context:
+                    logger.info(f"Created new context for patient {patient_id} and context form group {cfgs[0]}")
                     context = RDRFContext.objects.create(registry=self.registry_model,
                                                          content_type=content_type,
                                                          object_id=patient_id,
@@ -96,7 +102,7 @@ class Command(BaseCommand):
             for c in contexts:
                 if (patient.id, c.id) in seen:
                     continue
-                args.append((self.registry_model.code, patient.id, c.id))
+                args.append((self.registry_model.code, patient, c))
                 seen.add((patient.id, c.id))
 
         db.connections.close_all()
